883c58499390e8bafdcdbdac0ca0a6df5976c909
Your Name <nitish.bhide@googlemail.com>
2012-07-23 00:42:07 +0530

    files added

---
+++ demo.py
@@ -0,0 +1,23 @@
+import flask, flask.views
+import fbconsole
+app = flask.Flask(__name__)
+# Don't do this!
+app.secret_key = "nitish"
+fbconsole.AUTH_SCOPE = ['publish_stream', 'publish_checkins']
+fbconsole.APP_ID = '266828850095076'
+fbconsole.authenticate()
+class View(flask.views.MethodView):
+    def get(self):
+        return flask.render_template('index.html')
+        
+    def post(self):
+        result = flask.request.form['expression']
+	status = fbconsole.post('/me/feed', {'message':result})
+	likes = fbconsole.get('/'+status['id']+'/likes')
+        flask.flash(result)
+        return self.get()
+    
+app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
+
+app.debug = True
+app.run()
